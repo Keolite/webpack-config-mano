0fc828e99208bee0b0a99606168d49fd70fa150b
Keolite <e.baillysalins@outlook.fr>
2023-06-20 16:18:29 +0200

    Webbapck : Gestion de la page html pour qu'elle soit ajouter dans le dossier app lors de la compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry : {
@@ -8,5 +9,16 @@ module.exports = {
         filename : 'index-bundle.js',
         clean: true,
         path: path.resolve(__dirname, 'app')
-    }
-};
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            inject : true,
+            minify : false,
+            meta:{
+                viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+                description: 'Ceci est un page pour webpack'
+            }
+        })
+    ]
+    
+};  
