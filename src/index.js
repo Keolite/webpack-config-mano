3ca469e38c9e184801e8c48c91e950896d906305
Keolite <e.baillysalins@outlook.fr>
2023-06-22 16:32:54 +0200

    JS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './app.js';
+import './style.css';
+
+let a = 'lol';
+console.log(a);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizeWebpackPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry : {
@@ -10,15 +13,49 @@ module.exports = {
         clean: true,
         path: path.resolve(__dirname, 'app')
     },
+    module:{
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader,'css-loader']
+            },
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: 
+                        [
+                            ['@babel/preset-env', {targets: {"ie": "11"}}]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
     plugins : [
         new HtmlWebpackPlugin({
             inject : true,
-            minify : false,
+            minify : true,
             meta:{
                 viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
                 description: 'Ceci est un page pour webpack'
             }
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
         })
-    ]
+
+    ],
+    optimization :{
+        minimizer: [
+            new CssMinimizeWebpackPlugin(),
+            new TerserPlugin({
+                test: /\.js$/i,
+                exclude: /node_modules/
+            }),
+        ]
+    }
     
 };  
